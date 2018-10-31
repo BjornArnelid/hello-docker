8e71f3aef89a5919b8dbf547b634d9992f25b593
Björn Arnelid <bjorn.arnelid@gmail.com>
2018-10-31 11:42:18 +0100

    Hello world using Flask

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from flask import Flask
+
+# create a server object and register it with import name __name__ (so that extensions etc can identify Flask)
+server = Flask(__name__)
+
+
+# Assign a path to say_hello
+@server.route('/hello')
 def say_hello():
-    print("Hello World")
+    """Say hello"""
+    return "Hello World"
 
 
 if __name__ == '__main__':
-    say_hello()
+    # Start the server in standalone mode
+    server.run()
