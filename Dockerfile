7092e689be3706396dfb84c9acbeee698e0d6209
Björn Arnelid <bjorn.arnelid@gmail.com>
2018-10-31 14:21:17 +0100

    Fixing docker naming issue and adding hello to person

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # Build on a complete production ready nginx image
-FROM tiangolo/uwsgi-nginx-flask:python3.6
+FROM tiangolo/uwsgi-nginx-flask:python3.7
 
 # Copy the application to our docker image
 # it will start automatically since it has the default name main.py
-COPY . /app
+ADD . /app
+
+# To build using "docker build . -t hello-docker"
+# To run "docker run hello-docker"
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask
 
-# create a server object and register it with import name __name__ (so that extensions etc can identify Flask)
-server = Flask(__name__)
+# Create a server object and register it with import name __name__ (so that extensions etc can identify Flask)
+# Our docker image expects the flask instance to be called 'app'
+app = Flask(__name__)
 
 
 # Assign a path to say_hello
-@server.route('/hello')
+@app.route('/hello')
 def say_hello():
     """Say hello"""
-    return "Hello World"
+    return "Hello new World"
+
+
+# Assign new path to say hello to a specific person
+@app.route('/hello/<name>')
+def say_hello_to(name):
+    """Say hello to a specific person"""
+    return 'Hello %s' % name
 
 
 if __name__ == '__main__':
     # Start the server in standalone mode
-    server.run()
+    app.run()
